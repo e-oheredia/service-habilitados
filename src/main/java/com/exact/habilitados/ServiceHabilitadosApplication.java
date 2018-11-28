244be440d802f6ec2877aaeb092d892d52cffb6b
RonaldSantosR <rsantos@exact.com.pe>
2018-11-28 14:59:08 -0500

    Creacion de Proyectos

---
+++ src/main/java/com/exact/habilitados/ServiceHabilitadosApplication.java
@@ -0,0 +1,12 @@
+package com.exact.habilitados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHabilitadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHabilitadosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exact/habilitados/ServiceHabilitadosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exact.habilitados;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceHabilitadosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
